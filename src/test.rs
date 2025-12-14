0c4d0f0fb7f74be1835a1d2a15e52ddcdb96bf0c
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-12-14 10:03:53 +0800

    Rename

--- src/test.rs
+++ src/test.rs
@@ -4,7 +4,7 @@ use crate::privatekey::ECPrivateKey;
 #[test_case::test_case("ec384-cert.pem", "ec384-private.pem")]
 #[test_case::test_case("ec256-cert.pem", "ec256-private.pem")]
 #[test_case::test_case("ec521-cert.pem", "ec521-private.pem")]
-#[test_case::test_case("ec384-oldcert.pem", "ec384-oldprivatekey.pem")]
+#[test_case::test_case("ec384-oldcert.pem", "ec384-oldprivate.pem")]
 fn test_actual_file_ec(cert: &str, privkey: &str) -> anyhow::Result<()> {
     let private_key = ECPrivateKey::load_privatekey_pem(privkey).unwrap();
     let pubkey_from_priv = private_key.extract_publickey().unwrap();
