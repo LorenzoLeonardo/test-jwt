1ae29dc2ac52de60e428aba21f6539694c3f3650
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-12-14 08:31:46 +0800

    Hide OID for the signatures

--- src/display.rs
+++ src/display.rs
@@ -2,6 +2,8 @@ use std::fmt;
 
 use time::OffsetDateTime;
 use x509_parser::{
+    asn1_rs::Oid,
+    oid_registry,
     prelude::{FromDer, ParsedExtension, X509Certificate},
     time::ASN1Time,
 };
@@ -55,7 +57,7 @@ fn dump_cert(f: &mut fmt::Formatter<'_>, cert: &X509Certificate) -> fmt::Result 
     writeln!(
         f,
         "        Signature Algorithm: {}",
-        tbs.signature.algorithm.to_id_string()
+        signature_algorithm_name(&tbs.signature.algorithm)
     )?;
 
     // Issuer
@@ -83,14 +85,8 @@ fn dump_cert(f: &mut fmt::Formatter<'_>, cert: &X509Certificate) -> fmt::Result 
     writeln!(
         f,
         "            Public Key Algorithm: {}",
-        spki.algorithm.algorithm.to_id_string()
+        signature_algorithm_name(&spki.algorithm.algorithm)
     )?;
-
-    if let Some(params) = &spki.algorithm.parameters
-        && let Ok(oid) = params.as_oid()
-    {
-        writeln!(f, "                ASN1 OID: {}", oid.to_id_string())?;
-    }
 
     let pubkey = &spki.subject_public_key.data;
     writeln!(f, "                Public-Key: ({} bit)", pubkey.len() * 8)?;
@@ -99,6 +95,15 @@ fn dump_cert(f: &mut fmt::Formatter<'_>, cert: &X509Certificate) -> fmt::Result 
         writeln!(f, "                    {}", hex_colon(chunk))?;
     }
 
+    if let Some(params) = &spki.algorithm.parameters
+        && let Ok(oid) = params.as_oid()
+    {
+        writeln!(
+            f,
+            "                ASN1 OID: {}",
+            signature_algorithm_name(&oid)
+        )?;
+    }
     // Extensions
     writeln!(f, "        X509v3 extensions:")?;
     for ext in tbs.extensions().iter() {
@@ -139,7 +144,7 @@ fn dump_cert(f: &mut fmt::Formatter<'_>, cert: &X509Certificate) -> fmt::Result 
     writeln!(
         f,
         "    Signature Algorithm: {}",
-        cert.signature_algorithm.algorithm
+        signature_algorithm_name(&cert.signature_algorithm.algorithm)
     )?;
 
     writeln!(f, "    Signature Value:")?;
@@ -162,4 +167,38 @@ fn format_time(t: &ASN1Time) -> String {
     let dt: OffsetDateTime = t.to_datetime();
     dt.format(&time::format_description::well_known::Rfc2822)
         .unwrap()
+}
+
+pub fn signature_algorithm_name<'a>(oid: &Oid<'a>) -> String {
+    if *oid == oid_registry::OID_PKCS1_SHA1WITHRSA {
+        "sha1WithRSAEncryption".into()
+    } else if *oid == oid_registry::OID_PKCS1_SHA224WITHRSA {
+        "sha224WithRSAEncryption".into()
+    } else if *oid == oid_registry::OID_PKCS1_SHA256WITHRSA {
+        "sha256WithRSAEncryption".into()
+    } else if *oid == oid_registry::OID_PKCS1_SHA384WITHRSA {
+        "sha384WithRSAEncryption".into()
+    } else if *oid == oid_registry::OID_PKCS1_SHA512WITHRSA {
+        "sha512WithRSAEncryption".into()
+    } else if *oid == oid_registry::OID_SIG_ECDSA_WITH_SHA224 {
+        "ecdsa-with-SHA224".into()
+    } else if *oid == oid_registry::OID_SIG_ECDSA_WITH_SHA256 {
+        "ecdsa-with-SHA256".into()
+    } else if *oid == oid_registry::OID_SIG_ECDSA_WITH_SHA384 {
+        "ecdsa-with-SHA384".into()
+    } else if *oid == oid_registry::OID_SIG_ECDSA_WITH_SHA512 {
+        "ecdsa-with-SHA512".into()
+    } else if *oid == oid_registry::OID_SIG_DSA_WITH_SHA1 {
+        "dsa-with-SHA1".into()
+    } else if *oid == oid_registry::OID_KEY_TYPE_EC_PUBLIC_KEY {
+        "id-ecPublicKey".into()
+    } else if *oid == oid_registry::OID_EC_P256 {
+        "prime256v1".into()
+    } else if *oid == oid_registry::OID_NIST_EC_P384 {
+        "secp384r1".into()
+    } else if *oid == oid_registry::OID_NIST_EC_P521 {
+        "secp521r1".into()
+    } else {
+        oid.to_id_string()
+    }
 }
